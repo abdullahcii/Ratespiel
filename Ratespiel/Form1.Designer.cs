425144e27290d9bccd8bf3cc1b4f62b2fe6a3dc5
Abdullah Ciftci <abdullahciftci1515@gmail.com>
2022-12-11 17:29:23 +0100

    Projektdateien hinzufügen.

---
+++ Ratespiel/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Ratespiel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(295, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Prüfen";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Emoji", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(171, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(392, 33);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(131, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(529, 43);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ich denke an eine Zahl zwischen:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(106, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sie haben 0 mal geraten";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ratespiel/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ratespiel
+{
+    public partial class Form1 : Form
+    {
+
+
+        Random randomNumber = new Random();
+
+
+        int nummer = 0;
+        int schätzen = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            frageLaden();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox1.Text);
+            schätzen += 1;
+
+
+            label2.Text = "Sie haben " + schätzen + " geraten";
+
+            if(i == nummer)
+            {
+                MessageBox.Show("Richtig geratet!");
+                frageLaden();
+                textBox1.Text = "";
+                schätzen = 0;
+                    
+            }
+            else if (i < nummer)
+            {
+                MessageBox.Show("HÖHER GEHEN");
+            }
+            else
+            {
+                MessageBox.Show("KLEINER GEHEN");
+            }
+        }
+
+        private void frageLaden()
+        {
+            nummer = randomNumber.Next(0, 10);
+
+            label1.Text = "Ich denke an eine Zahl zwischen: 0 und 10 ";
+
+        }
+    }
+}
